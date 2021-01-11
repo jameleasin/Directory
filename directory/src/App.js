ecaf985b0aa5cb23730857aa2e79c0c00e2da846
Jamie <Sinclair>
2021-01-11 21:21:06 +0800

    dependancies upload

--- directory/src/App.js
+++ directory/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import EmployeeCard from './components/EmployeeCard';
-import SearchForm from './components/SeachForm';
+import SearchForm from './components/SearchForm';
 import Wrapper from './components/Wrapper';
 import Col from './components/Col';
 import API from './utils/API';
